98f2e42521108bb4c4cdaeef6334772d5c44204c
Kanika Rawat <114948582+CryptAura@users.noreply.github.com>
2023-08-21 13:48:27 +0530

    Create DAAQ2.c
    This program have quadratic time complexity, the CPU 0 seconds to execute.

---
+++ time.h/DAAQ2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+int main(){
+    clock_t Start,end;
+double CPU_time_used;
+Start=clock();
+int i,j,Sum=0;
+for (i=0;i<10;i++)
+{
+   for (j=0;j<10;j++){
+    Sum+=i;
+   }        //Program's complexity is Quadratic.
+
+}
+end=clock();
+CPU_time_used=((double)(end-Start))/CLOCKS_PER_SEC;
+ printf("CPU time used: %f seconds\n", CPU_time_used);
+}
